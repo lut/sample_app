0e5b0f0e316447cbb12d1834d964432698053a07
lut <lutgardo21@gmail.com>
2013-01-20 00:42:59 +0100

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :show, :edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
+
   def new
   	@user = User.new
   end
@@ -7,15 +11,67 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if  @user.update_attributes(params[:user])
+      flash[:succes] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def create
   	@user = User.new(params[:user])
   	if @user.save
       sign_in @user
   		flash[:success] = "Welcome to the Sample App!"
   		redirect_to @user
-	else
-		render 'new'
+	   else
+		  render 'new'
+	   end
 	end
-	end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:succes] = "User destroyed."
+    redirect_to users_url
+  end
+
+    
   
+
+  private 
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in." 
+  end
+
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin_user?
+  end
+      
+    end
+    
+  end
+  
+
+
+end
 end
