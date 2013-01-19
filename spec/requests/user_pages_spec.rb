0f23a8168f186c168b75bf727a75e0315e14500a
lutgarcia <lutgardo21@gmail.com>
2013-01-19 16:04:15 +0100

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -38,6 +38,11 @@ describe "signup" do
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
+      
+      describe "after saving the user"
+        it { should have_link('Sign out') }
+      end
+
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
