ba20e58066f4b4f48e3909bc69be4c32e93fcca9
lut <lutgardo21@gmail.com>
2013-01-20 09:57:30 +0100

    current_user.admin? method fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,7 +64,7 @@ class UsersController < ApplicationController
   end
 
   def admin_user
-    redirect_to(root_path) unless current_user.admin_user?
+    redirect_to(root_path) unless current_user.admin?
   end
   
 end
